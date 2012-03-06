cc6f100823ea9e8df38d3dbdd2149cb1c8275ad1
Thomas <thomas@thomas-linux.(none)>
2012-03-06 15:48:02 +0100

    page Discipline

--- miniProjetBdd/Discipline.php
+++ miniProjetBdd/Discipline.php
@@ -1,7 +1,74 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    require_once("header.php");
+    require_once("bdd.php");
+    print_header("Discipline", "Style.css", "Script.js");
 
 ?>
+<br/>
+<br/>
+
+<div id="mainContainer">
+    <br/><h3>Table DISCIPLINE</h3>
+    <?php
+    bdd_connect();
+    $req="Select * from DISCIPLINE";
+    $res=bdd_query($req);
+    echo"<div id=\"tableau\">\n
+        <table id=\"table\">\n
+            <tr>\n
+                <td><b>Identifiant</b></td><td class=\"libelleDiscipline\"><b>Libelle</b></td>\n
+             </tr>";
+
+    while($l=mysql_fetch_array($res)){
+        echo"<tr>\n
+                <td>".$l[0]."</td><td class=\"libelleDiscipline\">".$l[1]."</td>
+                <td><a href=\"javascript:void(0)\" onClick=\"deroule2(100, 1, '".$l[1]."', '".$l[0]."');remplir_champ_modif('no', ".$l[0].");remplir_champ_modif('libelleDisciplineModif', '".$l[1]."')\">
+                    <img src=\"img/Edit.png\" title=\"modifier\"></a>
+
+                <a href=\"javascript:if(confirm('confirmez-vous la suppression ?'))document.location.href='Maj_discipline.php?action=2&id=".$l[0]."'\" ><img src=\"img/remove.png\" title=\"supprimer\"></a></td>\n";
+
+    }
+    echo "</table></div>"
+    ?>
+
+    <div id="form_ajout">
+        <input type="button" onClick="deroule(100, 1)" value="Ajouter une discipline"><br/>
+        <form method="POST" action="Maj_discipline.php?action=1" onSubmit="return confirm('confirmez-vous l\'ajout ?')">
+               <table>
+                    <tr>
+                   <td><label for="libelleDiscipline">Nom Discipline : </label></td><td><input type="text" name="libelleDiscipline" id="libelleDiscipline" onKeyUp="check_field_nom_discipline('libelleDiscipline')"></td>
+                   <td id="error_libelleDiscipline"></td>
+                   </tr>
+                   <tr>
+                       <td></td>
+                       <td><input type="submit" value="Ajouter" onclick="return check_field_nom_discipline('libelleDiscipline')">
+                           <input type="reset" value="Annuler" onClick="deroule(28,2); field_ok('libelleDiscipline')"> </td>
+                   </tr>
+               </table>
+
+        </form>
+    </div><br/><br/>
+
+    <div id="form_modif">
+        <br/>
+        modifier la discipline
+        <form method="POST" action="Maj_discipline.php?action=3" onSubmit="return confirm('confirmez-vous la modification ?')">
+               <table>
+                   <tr>
+                   <td><input type="text" name="no" id="no" readonly="readonly"></td><td><input type="text" name="libelleDisciplineModif" id="libelleDisciplineModif" onKeyUp="check_field_nom_discipline('libelleDisciplineModif')"></td>
+                   <td id="error_libelleDisciplineModif"></td>
+                   </tr>
+                   <tr>
+                       <td></td>
+                       <td><input type="submit" value="Modifier" onClick="return check_field_nom_discipline('libelleDisciplineModif')">
+                         <input type="reset" value="Annuler" onClick="deroule2(0,2); field_ok('libelleDisciplineModif')"></td>
+                   </tr>
+               </table>
+
+
+        </form>
+    </div>
+</div><br/>
+
+</body>
+</html>
--- miniProjetBdd/Maj_discipline.php
+++ miniProjetBdd/Maj_discipline.php
@@ -1,7 +1,30 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    require_once 'bdd.php';
+    bdd_connect();
+    $action=$_GET["action"];
+
+    if($action=="1"){
+        $nom=$_POST["libelleDiscipline"];
+        $req="insert into DISCIPLINE values (null, '".$nom."')";
+        $res = bdd_query($req);
+    }
+
+    else if($action=="2"){
+        $id=$_GET["id"];
+        $req = "delete from DISCIPLINE where noDiscipline=".$id."";
+        $res = bdd_query($req);
+    }
+
+    else if($action=="3"){
+        $id=$_POST["no"];
+        $nom=$_POST["libelleDisciplineModif"];
+        var_dump($id);
+        var_dump($nom);
+        $req = "update DISCIPLINE set libelleDiscipline='".$nom."' where noDiscipline='".$id."'";
+        $res = bdd_query($req);
+    }
+    print ("<script language = \"JavaScript\">");
+    print ("location.href = 'Discipline.php';");
+    print ("</script>");
 
 ?>
